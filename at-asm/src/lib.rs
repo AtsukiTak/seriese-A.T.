6db475913c7f917897071f7925c41a6ea70f0ba5
AtsukiTak <takatomgoo@gmail.com>
2021-09-05 01:44:27 +0900

    ret命令のマシンコードをat-x64から取得

--- at-asm/src/lib.rs
+++ at-asm/src/lib.rs
@@ -2,6 +2,7 @@ pub mod reader;
 
 use crate::reader::Reader;
 use at_obj::{macho, Object, Symbol};
+use at_x64::instruction::Ret;
 use std::io::{Read, Write};
 
 pub fn assemble<R: Read, W: Write>(read: &mut R, write: &mut W) {
@@ -12,9 +13,6 @@ pub fn assemble<R: Read, W: Write>(read: &mut R, write: &mut W) {
         parse_line(line, &mut obj);
     }
 
-    // mov ax, 42
-    // ret
-    obj.sections.text.bytes = vec![0x66, 0xb8, 0x2a, 0x00, 0xc3];
     obj.sections.text.symbols = vec![Symbol::Ref {
         name: "_main".to_string(),
         addr: 0,
@@ -25,7 +23,10 @@ pub fn assemble<R: Read, W: Write>(read: &mut R, write: &mut W) {
 
 fn parse_line(line: &str, obj: &mut Object) {
     match line {
-        "ret\n" => obj.sections.text.bytes.push(0xc3),
+        "ret\n" => {
+            let bytes = Ret::new().bytecode().to_bytes();
+            obj.sections.text.bytes.extend_from_slice(bytes.bytes());
+        }
         "mov ax, 42\n" => obj
             .sections
             .text
